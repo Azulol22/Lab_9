4c2207c305725b7515ed22cc8c1c05b955aa3757
jkwli <wright_joshua@wheatoncollege.edu>
2025-04-16 16:00:46 -0400

    Algorith .cpp and .h file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "arithmetic.h"
+
+using namespace std;
 
 int main() {
 
+    cout << sum(9,5)<< endl;
+    cout << sub(9,5)<< endl;
+    cout << multiply(9,5)<< endl;
+    cout << divide(9,5)<< endl;
     return 0;
 }
